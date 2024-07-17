80df0011be81aa545491bdb24e436866d24462b0
Gautam, Aditya (IST) <Aditya.Gautam@riotinto.com>
2024-07-17 17:47:44 +1000

    some chnage

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import "./index.scss";
 import App from "./App";
 import { UserProvider } from "./context/user.context";
 import reportWebVitals from "./reportWebVitals";
+import { ProductsProvider } from "./context/products.context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
     <UserProvider>
+    <ProductsProvider>
       <App />
+      </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
