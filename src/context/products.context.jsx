b11f3fcf5fcffebafc8713354262f8b7cb483dea
Gautam, Aditya (IST) <Aditya.Gautam@riotinto.com>
2024-07-25 17:04:01 +1000

    get doc from firebase

--- src/context/products.context.jsx
+++ src/context/products.context.jsx
@@ -1,6 +1,6 @@
-import { createContext, useState } from "react";
-import PRODUCTS from '../shop-data.json';
-
+import { createContext, useState, useEffect } from "react";
+import SHOP_DATA from "../shop-data";
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
 
 export const ProductsContext = createContext({
     products: [],
@@ -8,10 +8,16 @@ export const ProductsContext = createContext({
 });
 
 export const ProductsProvider = ({ children }) => {
-   const [products, setProducts] = useState(PRODUCTS);
+   const [products, setProducts] = useState([]);
+  //  useEffect(() =>{addCollectionAndDocuments('categories', SHOP_DATA)},[])
+      useEffect(() =>{
+        // we dont make use effect asyn but to call a async method we do it like below
+        const getCategoriesMap= async() =>{
+       const categoryMap=  await   getCategoriesAndDocuments()}
+
+       getCategoriesMap();
+      },[])
    const value = { products, setProducts};
-
-
 
   return <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>;
 };
